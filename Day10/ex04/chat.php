e2fc22cd916d261df713866a107406f6143275d4
Sergiu Logos <slogos@e1z2r8p17.42.us.org>
2017-05-07 14:21:42 -0700

    learning php

--- Day10/ex04/chat.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (file_exists("../private/chat"))
-{
-	date_default_timezone_set("Europe/Paris");
-	$tab = unserialize(file_get_contents("../private/chat"));
-	foreach ($tab as $key =>$elem)
-		echo date("[H:i]", $elem['time'])." <b>".$elem['login']."</b>: ".$elem['msg']."<br />".PHP_EOL;
-}
-?>
--- Day10/ex04/logout.php
+++
@@ -1,4 +0,0 @@
-<?php
-session_start();
-$_SESSION['loggued_on_user'] = "";
-?>
